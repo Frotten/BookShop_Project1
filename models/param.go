a6165c1f4ce0cc23f5eeb7d88b54abf2a089dd3b
Frotten <frottenice@outlook.com>
2026-03-02 22:56:39 +0800

    Recover a little bug

--- models/param.go
+++ models/param.go
@@ -5,7 +5,8 @@ type ParamSignUp struct {
 	Password   string `json:"password" binding:"required"`                     //binding:"required"表示该字段为必填项
 	RePassword string `json:"re_Password" binding:"required,eqfield=Password"` //eqfield=Password表示该字段必须和Password字段相等
 	Email      string `json:"email" binding:"required"`                        //email表示该字段必须是一个合法的邮箱格式
-	Gender     int8   `json:"gender" binding:"required,oneof=0 1"`             //oneof=0 1表示只能是这两个值之一，0表示男，1表示女
+	Gender     int8   `json:"gender" binding:"oneof=0 1"`
+	//oneof=0 1表示只能是这两个值之一，0表示男，1表示女,不能有required，因为有默认值0，如果有required，就会导致gender字段无法省略，必须传入一个值，如果传入0，就会被认为是缺少gender字段，从而失败
 }
 
 type ParamLogin struct {
