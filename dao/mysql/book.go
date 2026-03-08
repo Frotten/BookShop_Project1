caa7b37dd199e462afde514f87f78072e88dc41c
Frotten <frottenice@outlook.com>
2026-03-08 20:51:43 +0800

    Add PageSelect

--- dao/mysql/book.go
+++ dao/mysql/book.go
@@ -2,6 +2,8 @@ package mysql
 
 import "Project1_Shop/models"
 
+const PageSize = 10
+
 func GetAllBooks() ([]*models.Book, error) {
 	var Books []*models.Book
 	result := DB.Find(&Books)
@@ -12,4 +14,13 @@ func GetAllBooks() ([]*models.Book, error) {
 		return nil, result.Error
 	}
 	return Books, nil
+}
+
+func GetBooksByPage(page int) ([]*models.Book, int64, error) {
+	var Books []*models.Book
+	var TotalPage int64
+	DB.Model(&models.Book{}).Count(&TotalPage)
+	offset := (page - 1) * PageSize
+	err := DB.Limit(PageSize).Offset(offset).Find(&Books).Error
+	return Books, TotalPage, err
 }
